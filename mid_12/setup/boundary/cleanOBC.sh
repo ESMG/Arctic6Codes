03b8d961d2c8d533a7a4a29eb473327555006cf7
Rob Cermak <rob.cermak@gmail.com>
2022-08-30 10:31:52 -0800

    OBC and forcing file updates

---
+++ mid_12/setup/boundary/cleanOBC.sh
@@ -0,0 +1,44 @@
+CMD=ls
+CMD=rm
+
+#${CMD} p2003/*_2003.nc p2003/regrid_segment_00*.nc
+#${CMD} p2004/*_2004.nc p2004/regrid_segment_00*.nc
+#${CMD} p2005/*_2005.nc p2005/regrid_segment_00*.nc
+#${CMD} p2006/*_2006.nc p2006/regrid_segment_00*.nc
+#${CMD} p2007/*_2007.nc p2007/regrid_segment_00*.nc
+#${CMD} p2008/*_2008.nc p2008/regrid_segment_00*.nc
+#${CMD} p2009/*_2009.nc p2009/regrid_segment_00*.nc
+#${CMD} p2010/*_2010.nc p2010/regrid_segment_00*.nc
+#${CMD} p2011/*_2011.nc p2011/regrid_segment_00*.nc
+#${CMD} p2012/*_2012.nc p2012/regrid_segment_00*.nc
+#${CMD} p2013/*_2013.nc p2013/regrid_segment_00*.nc
+#${CMD} p2014/*_2014.nc p2014/regrid_segment_00*.nc
+#${CMD} p2015/*_2015.nc p2015/regrid_segment_00*.nc
+#${CMD} p2016/*_2016.nc p2016/regrid_segment_00*.nc
+#${CMD} p2017/*_2017.nc p2017/regrid_segment_00*.nc
+#${CMD} p2018/*_2018.nc p2018/regrid_segment_00*.nc
+#${CMD} p2019/*_2019.nc p2019/regrid_segment_00*.nc
+#${CMD} p2020/*_2020.nc p2020/regrid_segment_00*.nc
+
+YR=2016
+${CMD} p${YR}_01/*_${YR}.nc p${YR}_01/regrid_segment_00*.nc
+${CMD} p${YR}_02/*_${YR}.nc p${YR}_02/regrid_segment_00*.nc
+${CMD} p${YR}_03/*_${YR}.nc p${YR}_03/regrid_segment_00*.nc
+${CMD} p${YR}_04/*_${YR}.nc p${YR}_04/regrid_segment_00*.nc
+${CMD} p${YR}_05/*_${YR}.nc p${YR}_05/regrid_segment_00*.nc
+${CMD} p${YR}_06/*_${YR}.nc p${YR}_06/regrid_segment_00*.nc
+${CMD} p${YR}_07/*_${YR}.nc p${YR}_07/regrid_segment_00*.nc
+${CMD} p${YR}_08/*_${YR}.nc p${YR}_08/regrid_segment_00*.nc
+exit
+${CMD} p${YR}_09/*_${YR}.nc p${YR}_09/regrid_segment_00*.nc
+${CMD} p${YR}_10/*_${YR}.nc p${YR}_10/regrid_segment_00*.nc
+${CMD} p${YR}_11/*_${YR}.nc p${YR}_11/regrid_segment_00*.nc
+${CMD} p${YR}_12/*_${YR}.nc p${YR}_12/regrid_segment_00*.nc
+${CMD} p${YR}_13/*_${YR}.nc p${YR}_13/regrid_segment_00*.nc
+${CMD} p${YR}_14/*_${YR}.nc p${YR}_14/regrid_segment_00*.nc
+${CMD} p${YR}_15/*_${YR}.nc p${YR}_15/regrid_segment_00*.nc
+${CMD} p${YR}_16/*_${YR}.nc p${YR}_16/regrid_segment_00*.nc
+${CMD} p${YR}_17/*_${YR}.nc p${YR}_17/regrid_segment_00*.nc
+${CMD} p${YR}_18/*_${YR}.nc p${YR}_18/regrid_segment_00*.nc
+${CMD} p${YR}_19/*_${YR}.nc p${YR}_19/regrid_segment_00*.nc
+${CMD} p${YR}_20/*_${YR}.nc p${YR}_20/regrid_segment_00*.nc
---
+++ mid_12/setup/boundary/invGlorys.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+# Perform an inventory of glorys files
+# DIR1 = /import/AKWATERS/kshedstrom/glorys
+# DIR2 = /import/AKWATERS/kshedstrom/glorys2
+
+# Scan for which years are present
+# Example:
+# /import/AKWATERS/kshedstrom/glorys/GLORYS_REANALYSIS_1993-01-01.nc
+
+import os, sys, glob
+import datetime
+
+years_present = []
+sdir = []
+
+# Directory 1
+sdir.append('/import/AKWATERS/kshedstrom/glorys')
+in_files = glob.glob(os.path.join(sdir[0], "*.nc"))
+in_files.sort()
+for inf in in_files:
+    #breakpoint()
+    fyr = os.path.basename(inf)[18:22]
+    if not(fyr) in years_present:
+        years_present.append(fyr)
+
+# Directory 2
+sdir.append('/import/AKWATERS/kshedstrom/glorys2')
+in_files = glob.glob(os.path.join(sdir[1], "*.nc"))
+in_files.sort()
+for inf in in_files:
+    fyr = os.path.basename(inf)[18:22]
+    if not(fyr) in years_present:
+        years_present.append(fyr)
+
+print("Scanning GLORYS for missing files by year...")
+#print(years_present)
+
+for yr in years_present:
+    beg = datetime.datetime(int(yr), 1, 1)
+    dt = beg
+    end = datetime.datetime(int(yr)+1, 1, 1)
+    missing = []
+    while dt < end:
+        #breakpoint()
+        for pth in sdir:
+            fn = dt.strftime('GLORYS_REANALYSIS_%Y-%m-%d.nc')
+            fp = os.path.join(pth, fn)
+            if not(os.path.isfile(fp)):
+                missing.append(fp)
+        dt = dt + datetime.timedelta(days=1)
+
+    #breakpoint()
+    if len(missing) > 0:
+        print(yr)
+        missing.sort()
+        for fn in missing:
+            print("  > %s" % (fn))
+        print("  ")
